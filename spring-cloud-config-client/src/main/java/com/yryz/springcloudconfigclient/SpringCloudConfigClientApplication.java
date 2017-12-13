d82675d5ec593ea0ffad4d030262c4ee09a0cbec
pengnian <pengnian@yryz.com>
2017-12-13 10:28:51 +0800

    git上的配置改变利用消息总线热更新服务配置

--- spring-cloud-config-client/src/main/java/com/yryz/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/yryz/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -3,12 +3,14 @@ package com.yryz.springcloudconfigclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class SpringCloudConfigClientApplication {
 
     public static void main(String[] args) {
